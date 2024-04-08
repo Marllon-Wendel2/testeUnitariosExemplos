33096bdbfcc87b6e9e64d8c4b137985c3dfc91c8
Marllon Wendel <m.w.p.ferreira@outlook.com>
2024-04-08 14:41:17 -0300

    novos testes

--- carrinho/test/carrinho.test.js
+++ carrinho/test/carrinho.test.js
@@ -5,5 +5,32 @@ describe('Teste do Carrinho', () => {
   it('Deve iniciar vazio', () => {
     const carrinho = new Carrinho();
     expect(carrinho.subtotal).toBeNull();
-});
+  });
+
+  it('Deve ter itens', () => {
+    const item = new Item('Banana', 2, 5);
+    const item2 = new Item('Maca', 0.5, 1);
+
+    const carrinho = new Carrinho();
+    carrinho.adiciona(item);
+    carrinho.adiciona(item2);
+
+    expect(typeof carrinho).toBe('object');
+    expect(carrinho.itens[0]).toBe(item);
+    expect(carrinho.itens[1]).toBe(item2);
+  });
+
+  it('Deve ter a propriedade total', () => {
+    const carrinho = new Carrinho();
+    expect(carrinho).toHaveProperty('total');
+  });
+
+  it('Deve lançar erro ao finalizar compra com carrinho vazio', () => { 
+
+    function englobaErroCarrinho() {
+      const carrinho = new Carrinho();
+      carrinho.finalizaCompra();
+    }
+
+  });
 });
